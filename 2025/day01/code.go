8b707190898d48d4794f44f9ce477c6e5d46c3ca
Cory Jacobsen <cory@unrolled.ca>
2025-11-27 22:35:09 +0000

    prep for 2025

---
+++ 2025/day01/code.go
@@ -0,0 +1,35 @@
+package day01
+
+func PartOne() int {
+	// 	INPUT = ``
+
+	return 0
+}
+
+func PartTwo() int {
+	// 	INPUT = ``
+
+	return PartOne()
+}
+
+// func getNum(input string) int {
+// 	numbers := []int{}
+// 	for _, char := range input {
+// 		num, err := strconv.Atoi(string(char))
+// 		if err == nil {
+// 			numbers = append(numbers, num)
+// 		}
+// 	}
+
+// 	strResult := fmt.Sprintf("%d%d", numbers[0], numbers[len(numbers)-1])
+// 	return convertToNum(strResult)
+// }
+
+// func convertToNum(input string) int {
+// 	result, err := strconv.Atoi(input)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	return result
+// }
---
+++ 2025/day01/input.go
@@ -0,0 +1,5 @@
+package day01
+
+const Title = "Day 1: TBD"
+
+var INPUT = ``
