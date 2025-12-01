9c3087f2b8987a6c6ca20beff89c3c2e531adf8c
Cory Jacobsen <cory@unrolled.ca>
2025-12-01 19:13:46 +0000

    day01

--- 2025/day01/code.go
+++ 2025/day01/code.go
@@ -1,35 +1,100 @@
 package day01
 
-func PartOne() int {
-	// 	INPUT = ``
+import (
+	"strconv"
+	"strings"
+)
 
-	return 0
+const dialStartPos = 50
+
+func PartOne() int {
+	// INPUT = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+
+	zeros := 0
+	currentPos := dialStartPos
+	lines := strings.Split(INPUT, "\n")
+
+	for _, line := range lines {
+		var val int
+
+		if num, found := strings.CutPrefix(line, "L"); found {
+			val = convertToNum(num)
+			val = val * -1
+		} else {
+			val = convertToNum(strings.TrimPrefix(line, "R"))
+		}
+
+		currentPos = currentPos + val
+
+		for currentPos >= 100 {
+			currentPos -= 100
+		}
+
+		for currentPos < 0 {
+			currentPos += 100
+		}
+
+		if currentPos == 0 {
+			zeros++
+		}
+	}
+
+	return zeros
 }
 
 func PartTwo() int {
-	// 	INPUT = ``
+	// INPUT = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
 
-	return PartOne()
+	zeros := 0
+	currentPos := dialStartPos
+	lines := strings.Split(INPUT, "\n")
+
+	for _, line := range lines {
+		var val int
+
+		if num, found := strings.CutPrefix(line, "L"); found {
+			val = convertToNum(num)
+
+			for val != 0 {
+				currentPos--
+				val--
+
+				if currentPos == 0 {
+					zeros++
+				}
+
+				if currentPos == -1 {
+					currentPos = 99
+				}
+			}
+		} else {
+			val = convertToNum(strings.TrimPrefix(line, "R"))
+
+			for val < 0 {
+				val += 100
+				zeros++
+			}
+
+			for val != 0 {
+				currentPos++
+				val--
+
+				if currentPos == 100 {
+					currentPos = 0
+					zeros++
+				}
+			}
+		}
+	}
+
+	return zeros
 }
 
-// func getNum(input string) int {
-// 	numbers := []int{}
-// 	for _, char := range input {
-// 		num, err := strconv.Atoi(string(char))
-// 		if err == nil {
-// 			numbers = append(numbers, num)
-// 		}
-// 	}
+func convertToNum(input string) int {
+	result, err := strconv.Atoi(input)
+	if err != nil {
+		panic(err)
+	}
 
-// 	strResult := fmt.Sprintf("%d%d", numbers[0], numbers[len(numbers)-1])
-// 	return convertToNum(strResult)
-// }
-
-// func convertToNum(input string) int {
-// 	result, err := strconv.Atoi(input)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	return result
-// }
+	return result
+}
